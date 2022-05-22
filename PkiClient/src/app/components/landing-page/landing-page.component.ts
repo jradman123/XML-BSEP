be260eb70790a2874d41057a55fa95892a09265b
Jelena Radman <73402841+jradman12@users.noreply.github.com>
2022-05-22 21:42:44 +0200

    login fixed,data about loggedUser stored in localStorage

--- PkiClient/src/app/components/landing-page/landing-page.component.ts
+++ PkiClient/src/app/components/landing-page/landing-page.component.ts
@@ -3,6 +3,7 @@ import { NgForm } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/services/UserService/user.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
@@ -11,7 +12,7 @@ import { UserService } from 'src/app/services/UserService/user.service';
 export class LandingPageComponent implements OnInit {
 
   sub! : Subscription;
-  constructor(private authService: UserService,private _router: Router,) { 
+  constructor(private authService: UserService,private _router: Router,private _snackBar: MatSnackBar) { 
     
   }
 
@@ -19,36 +20,25 @@ export class LandingPageComponent implements OnInit {
   }
   onSubmit(f: NgForm) {
 
-    const loginObserver = {
-      next: (x: any) => {
-        console.log(x);
-        console.log("uuuuuuuuuuu");
-        if(x === "user"){
-          this._router.navigate(['/chome']);
-          console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-        }else {this._router.navigate(['/ahome']);
-        console.log("bbbbbbbbbbbbbbbbbbbb");
-        }
-
-      },
-      error: (err: any) => {
-        console.log(err);
-      },
-    };
-    this.authService.login(f.value).subscribe({
-      next: (x:any) =>{
-        localStorage.setItem('email',x.email);
-        localStorage.setItem('role',x.role);
-        console.log(x);
-        if(x.role === "user"){
-          this._router.navigate(['/chome']);
-          console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-        }else {this._router.navigate(['/ahome']);
-        console.log("bbbbbbbbbbbbbbbbbbbb");
-        }
-      }
-    }
-    );
+      const loginObserver = {
+        next: (x:any) => {
+           this._snackBar.open("     Welcome","Dismiss");
+           if(localStorage.getItem('role') == "Admin"){
+             this._router.navigate(['/ahome']);
+             
+           }
+           else {
+             this._router.navigate(['/chome']);
+ 
+          }
+        },
+         error: (err:any) => {
+           this._snackBar.open("Email or password are incorrect.Try again,please.","Dismiss"); 
+         
+         }};
+      
+      this.authService.login(f.value).subscribe(loginObserver);
+     }
   }
   // login(f: NgForm) {
 
@@ -65,4 +55,4 @@ export class LandingPageComponent implements OnInit {
   //   };
   //   this.authService.login(f.value).subscribe(loginObserver);
   // }
-}
+
--- PkiClient/src/app/services/UserService/user.service.ts
+++ PkiClient/src/app/services/UserService/user.service.ts
@@ -1,15 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 import { LogedUser } from 'src/app/interfaces/loged-user';
 import { SubjectData } from 'src/app/interfaces/subject-data';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private _http: HttpClient) { }
+  private currentUserSubject: BehaviorSubject<LogedUser>;
+  public currentUser: Observable<LogedUser>;
+  private user! : LogedUser;
+
+  constructor(private _http: HttpClient) { 
+    this.currentUserSubject = new BehaviorSubject<LogedUser>(
+      JSON.parse(localStorage.getItem('currentUser')!)
+    );
+    this.currentUser = this.currentUserSubject.asObservable();
+
+  }
 
   createSubject(newSubject: SubjectData): Observable<any> {
     return this._http.post<any>(
@@ -18,7 +29,18 @@ export class UserService {
     );
   }
 
-  login(model: LogedUser): any {
-    return this._http.post('http://localhost:8443/api/login', model);
+  login(model: any): Observable<LogedUser> {
+    return this._http.post(`http://localhost:8443/api/login`, model).pipe(
+      map((response: any) => {
+        if (response && response.token) {
+          localStorage.setItem('token', response.token.accessToken);
+          localStorage.setItem('currentUser', JSON.stringify(response));
+          localStorage.setItem('role' ,response.role)
+          localStorage.setItem('email' ,response.email)
+          this.currentUserSubject.next(response);
+        }
+        return this.user;
+      })
+    );
   }
 }
