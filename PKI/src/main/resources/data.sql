743d828b29311aabbc24f25d690e9e14ef7db107
Jelena Radman <73402841+jradman12@users.noreply.github.com>
2022-05-23 08:22:10 +0200

    seed db

--- PKI/src/main/resources/data.sql
+++ PKI/src/main/resources/data.sql
@@ -4,8 +4,14 @@ insert into authorities(id, name) values (3, 'USER_INTERMEDIATE');
 insert into authorities(id, name) values (4, 'USER_END_ENTITY');
 
 -- role: admin, user_root, user_ica, user_ee
-
-INSERT INTO public.users(email, is_activated, password, recovery_email, role)
-VALUES ( 'admin123@gmail.com', true, '$2a$10$C8y2GXceEjgc48nr1r5rt.KP0m1OBEfubQbagvEYDr/2DQE0Bddka', 'psw.company2@gmail.com', 0);
+--password AdminUser123!
+INSERT INTO public.users(
+    common_name, country, email, is_activated, locality, organization, organization_unit, password, recovery_email, role)
+    VALUES ('Admin Main', 'SRB', 'admin123@gmail.com', true, 'NS', 'UNS', 'FTN', '$2a$10$RIVr/hKkPlf/fq/INofwse926OZPkElV9HXnPwCmnUZHiHgXtniim', 'psw.company2@gmail.com', 0);
+--password UserAdmin123!
+INSERT INTO public.users(
+    common_name, country, email, is_activated, locality, organization, organization_unit, password, recovery_email, role)
+    VALUES ('Marko Markovic', 'SRB', 'markomarkovic123@gmail.com', true, 'NS', 'UNS', 'FTN', '$2a$10$.sq6OHYCOBy3nDql82vsX.E0srnWWHZwSD31SJmAm02Xku5fZ8k9y', 'raandmjenale@gmail.com', 1);
 
 INSERT INTO public.user_authorities(user_id, authority_id) VALUES (1, 1);
+INSERT INTO public.user_authorities(user_id, authority_id) VALUES (2, 4);
