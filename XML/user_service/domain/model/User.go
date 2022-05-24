07419467ec91085eaac61e75bd8072d2f706418b
Jelena Stajic <73436129+jelenaCleo@users.noreply.github.com>
2022-05-25 00:44:03 +0200

    Added field to user

--- XML/user_service/domain/model/User.go
+++ XML/user_service/domain/model/User.go
@@ -6,16 +6,17 @@ import (
 )
 
 type User struct {
-	ID          uuid.UUID `json:"id" gorm:"index:idx_name,unique"`
-	Username    string    `json:"username" gorm:"unique;not null"`
-	Password    string    `json:"password" gorm:"not null"`
-	Email       string    `json:"email" gorm:"type-varchar(100);unique;not null"`
-	PhoneNumber string    `json:"phoneNumber" gorm:"not null"`
-	FirstName   string    `json:"firstName" gorm:"not null"`
-	LastName    string    `json:"lastName" gorm:"not null"`
-	Gender      Gender    `json:"gender" gorm:"not null"`
-	Role        Role      `json:"role" gorm:"not null"`
-	Salt        string    `json:"salt" gorm:"not null"`
-	IsConfirmed bool      `json:"is_confirmed" gorm:"not null"`
-	DateOfBirth time.Time `json:"dateOfBirth" gorm:"not null"`
+	ID            uuid.UUID `json:"id" gorm:"index:idx_name,unique"`
+	Username      string    `json:"username" gorm:"unique;not null"`
+	Password      string    `json:"password" gorm:"not null"`
+	Email         string    `json:"email" gorm:"type-varchar(100);unique;not null"`
+	PhoneNumber   string    `json:"phoneNumber" gorm:"not null"`
+	FirstName     string    `json:"firstName" gorm:"not null"`
+	LastName      string    `json:"lastName" gorm:"not null"`
+	Gender        Gender    `json:"gender" gorm:"not null"`
+	Role          Role      `json:"role" gorm:"not null"`
+	Salt          string    `json:"salt" gorm:"not null"`
+	IsConfirmed   bool      `json:"is_confirmed" gorm:"not null"`
+	DateOfBirth   time.Time `json:"dateOfBirth" gorm:"not null"`
+	RecoveryEmail string    `json:"recovery_email" gorm:"typevarchar(100);unique;not null"`
 }
--- XML/user_service/infrastructure/api/user_mapper.go
+++ XML/user_service/infrastructure/api/user_mapper.go
@@ -7,14 +7,17 @@ import (
 
 func MapProduct(user *model.User) *pb.User {
 	usersPb := &pb.User{
-		Username:    user.Username,
-		Password:    user.Password,
-		Email:       user.Email,
-		PhoneNumber: user.PhoneNumber,
-		FirstName:   user.FirstName,
-		LastName:    user.LastName,
-		Gender:      string(user.Gender),
-		DateOfBirth: user.DateOfBirth.String(),
+		Username:      user.Username,
+		Password:      user.Password,
+		Email:         user.Email,
+		PhoneNumber:   user.PhoneNumber,
+		FirstName:     user.FirstName,
+		LastName:      user.LastName,
+		Gender:        string(user.Gender),
+		Role:          string(user.Role),
+		DateOfBirth:   user.DateOfBirth.String(),
+		RecoveryEmail: user.RecoveryEmail,
+		IsConfirmed: user.IsConfirmed
 	}
 	return usersPb
 }
