7b20f0a623a085f474375b3814e34ae41a62ae41
momoirox <majablgic505@gmail.com>
2022-05-31 22:25:13 +0200

    Frontend startup

--- Noberty/src/app/app.module.ts
+++ Noberty/src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { UserLandingPageComponent } from './pages/user-landing-page/user-landing-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { UnauthHeaderComponent } from './components/unauth-header/unauth-header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AuthHeaderComponent } from './components/auth-header/auth-header.component';
+import { MatCardModule } from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterPageComponent,
+    LoginPageComponent,
+    UserLandingPageComponent,
+    HomePageComponent,
+    UnauthHeaderComponent,
+    FooterComponent,
+    AuthHeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatCardModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
