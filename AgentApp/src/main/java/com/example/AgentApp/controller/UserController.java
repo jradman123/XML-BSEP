48422d1d882fc39372d9bcb1970f18e283b82911
Jelena Radman <73402841+jradman12@users.noreply.github.com>
2022-06-01 18:54:21 +0200

    showing personal user info

--- AgentApp/src/main/java/com/example/AgentApp/controller/UserController.java
+++ AgentApp/src/main/java/com/example/AgentApp/controller/UserController.java
@@ -2,16 +2,16 @@ package com.example.AgentApp.controller;
 
 
 import com.example.AgentApp.dto.ChangePasswordDto;
+import com.example.AgentApp.dto.UserInformationResponseDto;
+import com.example.AgentApp.mapper.UserMapper;
+import com.example.AgentApp.model.User;
 import com.example.AgentApp.security.TokenUtils;
 import com.example.AgentApp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
@@ -26,11 +26,25 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private UserMapper userMapper;
+
+    @CrossOrigin(origins = "http://localhost:4200")
     @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('REGISTERED_USER') or hasAuthority('OWNER')")
     @PutMapping(value = "/changePassword")
     public ResponseEntity<HttpStatus> changePassword(@Valid @RequestBody ChangePasswordDto changePasswordDto, HttpServletRequest request) {
         String token = tokenUtils.getToken(request);
         userService.changePassword(tokenUtils.getUsernameFromToken(token), changePasswordDto);
         return ResponseEntity.noContent().build();
+    }
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('REGISTERED_USER') or hasAuthority('OWNER')")
+    @GetMapping (value = "/getUserInformation")
+    public ResponseEntity<?> getUserInformation(HttpServletRequest request) {
+        String token = tokenUtils.getToken(request);
+        String username=tokenUtils.getUsernameFromToken(token);
+        User user = userService.findByUsername(username);
+        return new ResponseEntity<UserInformationResponseDto>(userMapper.mapToDto(user),HttpStatus.OK);
     }
 }
--- Noberty/src/app/app-routing.module.ts
+++ Noberty/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CompaniesListComponent } from './pages/companies-list/companies-list.component';
+
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
