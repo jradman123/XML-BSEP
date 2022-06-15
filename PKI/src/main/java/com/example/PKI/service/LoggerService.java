954c2f1c2244c20783f7d1ebc5d10656c4ada366
Jelena Radman <73402841+jradman12@users.noreply.github.com>
2022-06-15 23:11:56 +0200

    added logs in user controller - pki

---
+++ PKI/src/main/java/com/example/PKI/service/LoggerService.java
@@ -0,0 +1,21 @@
+package com.example.PKI.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface LoggerService {
+    void loginFailed(String email,String ipAddress);
+    void loginSuccess(String email);
+    void signUpFailed(String ipAddress);
+    void signUpSuccess(String email,String ipAddress);
+    void confirmAccountFailed(String email,String ipAddress);
+    void confirmAccountSuccess(String email,String ipAddress);
+    void changePasswordSuccess(String email,String ipAddress);
+    void sendCodeFailed(String email);
+    void sendCodeSuccess(String email);
+    void checkCodeFailed(String email,String ipAddress);
+    void checkCodeSuccess(String email,String ipAddress);
+    void resetPasswordSuccess(String email,String ipAddress);
+    void checkRecoveryEmailFailed(String email,String ipAddress);
+    void checkRecoveryEmailSuccess(String email,String ipAddress);
+}
