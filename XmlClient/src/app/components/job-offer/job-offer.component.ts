7b6f59fd0406766f9694b20fc5906b4781c653b0
Jelena Radman <73402841+jradman12@users.noreply.github.com>
2022-06-30 12:47:11 +0200

    showing job offers

--- XmlClient/src/app/components/job-offer/job-offer.component.ts
+++ XmlClient/src/app/components/job-offer/job-offer.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { JobOffer } from 'src/app/interfaces/job-offer';
+import { AuthService } from 'src/app/services/auth-service/auth.service';
 import { JobOfferService } from 'src/app/services/job-offer-service/job-offer.service';
 
 @Component({
@@ -12,10 +13,13 @@ export class JobOfferComponent implements OnInit {
 
   jobOffers! : JobOffer[];
   searchText = '';
+  username : any;
 
   constructor(
-    private jobOfferService : JobOfferService
-  ) { }
+    private jobOfferService : JobOfferService, private router : Router, private authService : AuthService
+  ) {
+      this.username = localStorage.getItem('username');
+   }
 
   ngOnInit(): void {
     this.jobOfferService.getAllJobOffers().subscribe(
@@ -23,6 +27,19 @@ export class JobOfferComponent implements OnInit {
         this.jobOffers = res.JobOffers
       }
     )
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['login']);
+  }
+
+  showProfile() {
+    this.router.navigate(['myProfile']);
+  }
+
+  openCreateJobOffer() {
+    this.router.navigate(['newJobOffer']);
   }
 
   
