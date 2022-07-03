cfa4b0b1a7772b57a6f55db8b295ddabd9875c92
Jelena Stajic <73436129+jelenaCleo@users.noreply.github.com>
2022-07-03 21:26:19 +0200

    docker working

--- XML/message_service/startup/config/config.go
+++ XML/message_service/startup/config/config.go
@@ -18,7 +18,7 @@ type Config struct {
 func NewConfig() *Config {
 
 	return &Config{
-		Port:               os.Getenv("POST_SERVICE_PORT"),
+		Port:               os.Getenv("MESSAGE_SERVICE_PORT"),
 		MessageDBHost:      os.Getenv("MESSAGE_DB_HOST"),
 		MessageDBPort:      os.Getenv("MESSAGE_DB_PORT"),
 		PublicKey:          "PostService",
